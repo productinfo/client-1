d6a6457982ce4bf57008815c52e55c60eb101f5f
Pedro Piñera <pepibumur@gmail.com>
2017-11-22 08:44:00 +0100

    Create project

---
+++ Sources/Client.swift
@@ -0,0 +1,9 @@
+//
+//  Client.swift
+//  PPinera
+//
+//  Created by Pedro Piñera on 22.11.17.
+//  Copyright © 2017 PPinera. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/ClientTests/ClientTests.swift
@@ -0,0 +1,23 @@
+//
+//  ClientTests.swift
+//  PPinera
+//
+//  Created by Pedro Piñera on 22.11.17.
+//  Copyright © 2017 PPinera. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Client
+
+class ClientTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(Client().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
